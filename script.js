c57a713b0da875eff70768a115d17a36f91883bf
TanerTalas <r7byx0@gmail.com>
2025-08-02 08:07:39 +0300

    feat: add forgot password step 2 skeleton, disable autocomplete, implement code input spacing
    - Added the basic structure and styling for "Forgot Password - Step 2"\n- Disabled autocomplete on all input fields for consistency\n- Implemented JavaScript logic for automatic spacing in the 6-digit code input

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ togglePassBtn.addEventListener('click', () => {
 
   togglePassIcon.alt = isPassword ? 'hide password icon' : 'show password icon';
 });
+
+const codeInput = document.getElementById("verification-code");
+
+codeInput.addEventListener("input", (e) => {
+  // Sadece rakamları al
+  const numbers = e.target.value.replace(/\D/g, "").slice(0, 6).split("");
+  
+  // Boşlukla birleştir
+  e.target.value = numbers.join(" ");
+});
