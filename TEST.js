d33a70aa6abef6f93a870c6cc297632d00437887
R7by <r7byx0@gmail.com>
2026-01-23 15:11:29 +0300

    Animation CSS rules were created in the test files and their structure was implemented in test.html.

--- TEST.js
+++ TEST.js
@@ -1,11 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Theme button function
-  const themeButton = document.getElementById("themeButton");
-  const html = document.documentElement;
-
-  themeButton.addEventListener("click", () => {
-    html.classList.toggle("dark");
-  });
-
-  
+ 
 });
